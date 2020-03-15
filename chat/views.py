1559f3e3dd9a0bb8e8608940f28dafbc2ad44a35
= <=>
2020-03-15 10:52:52 +0900

    20200315

---
+++ chat/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html', {})
+
+def room(request, room_name):
+    return render(request, 'room.html', {
+        'room_name': room_name
+    })
