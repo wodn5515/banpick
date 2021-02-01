7dd4cc8b5f8bd3b867b6d0d647f1330bd84c023b
wodn5515 <wodn5515@naver.com>
2021-02-01 12:53:43 +0900

    Fix : misspell

--- draft/utils.py
+++ draft/utils.py
@@ -2,12 +2,13 @@ from django.http import HttpResponse
 
 def lane_choice_done(request, draft):
     banpick = (draft.banpick_final.split('/') if draft.banpick_final else draft.banpick.split('/'))
-    team = request.POST.get("teat",None)
+    team = request.POST.get("team",None)
     team_od = (([6,9,10,17,18] if team == "blue" else [7,8,11,16,19]) if team else [6,9,10,17,18,7,8,11,16,19])
     cp_arr = [] # 챔피언 번호 리스트
     for i in team_od:
         cp_arr.append(banpick[i])
     if team:
+        cnt = 0
         od_arr = [] # 픽순 라인 리스트
         temp_dic = {} # 픽 임시(픽순)
         for i in range(5):
